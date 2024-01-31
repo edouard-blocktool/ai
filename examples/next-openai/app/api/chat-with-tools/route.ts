00a47b8129631cea0e4360a6393538b219ab4f11
Edouard Godfrey <edouard@fintool.com>
2024-01-30 17:42:03 -0800

    Highlight bug

--- examples/next-openai/app/api/chat-with-tools/route.ts
+++ examples/next-openai/app/api/chat-with-tools/route.ts
@@ -6,6 +6,7 @@ import {
   experimental_StreamData,
 } from 'ai';
 import OpenAI from 'openai';
+import { Stream } from 'openai/streaming';
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
@@ -63,60 +64,106 @@ const tools: Tool[] = [
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const model = 'gpt-3.5-turbo-0613';
+  const model = 'gpt-4-1106-preview';
 
-  const response = await openai.chat.completions.create({
-    model,
-    stream: true,
-    messages,
-    tools,
-    tool_choice: 'auto',
-  });
+  const WITH_THIS_QUESTION_STREAMING_OF_TOOL_CALL_WORKS = "What was $MSFT revenue in Q1 2022?";
+  const WITH_THIS_QUESTION_STREAMING_OF_TOOL_CALL_DOES_NOT_WORK = "Compare $MSFT revenue in Q1 2022 and Q1 2023?";
 
-  const data = new experimental_StreamData();
-  const stream = OpenAIStream(response, {
-    experimental_onToolCall: async (
-      call: ToolCallPayload,
-      appendToolCallMessage,
-    ) => {
-      for (const toolCall of call.tools) {
-        // Note: this is a very simple example of a tool call handler
-        // that only supports a single tool call function.
-        if (toolCall.func.name === 'get_current_weather') {
-          // Call a weather API here
-          const weatherData = {
-            temperature: 20,
-            unit: toolCall.func.arguments.format === 'celsius' ? 'C' : 'F',
-          };
-
-          const newMessages = appendToolCallMessage({
-            tool_call_id: toolCall.id,
-            function_name: 'get_current_weather',
-            tool_call_result: weatherData,
-          });
-
-          return openai.chat.completions.create({
-            messages: [...messages, ...newMessages],
-            model,
-            stream: true,
-            tools,
-            tool_choice: 'auto',
-          });
-        }
+  const params: any = {
+    "model": "gpt-4-1106-preview",
+    "messages": [
+      {
+        "content": "You are a professional financial analyst.\n\nCurrent date: 2024-01-31",
+        "role": "system"
+      },
+      {
+        "content": WITH_THIS_QUESTION_STREAMING_OF_TOOL_CALL_DOES_NOT_WORK,
+        "role": "user"
       }
-    },
+    ],
+    "temperature": 0.3,
+    "tools": [
+      {
+        "type": "function",
+        "function": {
+          "name": "search_filing",
+          "parameters": {
+            "type": "object",
+            "properties": {
+              "document_type": {
+                "type": "string",
+                "description": "SEC form type. For example \"10-K\", \"10-Q\", ..."
+              },
+              "section": {
+                "type": "string",
+                "description": "SEC form subsection like \"Item 1. Business\", \"Item 1A. Risk Factors\", ..."
+              },
+              "ticker": {
+                "type": "string",
+                "description": "Stock ticker to filter by."
+              },
+              "query": {
+                "type": "string",
+                "description": "Search query."
+              },
+              "keywords": {
+                "type": "array",
+                "description": "List of keywords that are likely to appear in the SEC form close to the answer.",
+                "items": {
+                  "type": "string"
+                }
+              },
+              "period": {
+                "type": "object",
+                "description": "Specify the period for the financial document. Either choose latest or specify fiscal_year and fiscal_quarter.",
+                "properties": {
+                  "latest": {
+                    "type": "boolean",
+                    "description": "If true, only search in the most recent document. This option is mutually exclusive with fiscal_year and fiscal_quarter."
+                  },
+                  "fiscal_year": {
+                    "type": "integer",
+                    "description": "Fiscal year of the document."
+                  },
+                  "fiscal_quarter": {
+                    "type": "integer",
+                    "description": "Fiscal quarter of the document (1-4). Use this in combination with fiscal_year when the user asks for a specific quarter."
+                  }
+                }
+              }
+            },
+            "required": [
+              "keywords"
+            ]
+          },
+          "description": "Perform a full text search on SEC documents of publicly traded companies. The function returns up to 5 results."
+        }
+      },
+    ],
+    "max_tokens": 1000,
+    "tool_choice": "auto",
+    "stream": true
+  }
+
+  const response: any = await openai.chat.completions.create(params);
+  // const response = await openai.chat.completions.create({
+  //   model,
+  //   stream: true,
+  //   messages,
+  //   tools,
+  //   tool_choice: 'auto',
+  // });
+
+  const stream = OpenAIStream(response, {
     onCompletion(completion) {
       console.log('completion', completion);
     },
     onFinal(completion) {
-      data.close();
     },
-    experimental_streamData: true,
+    onToken(token) {
+      console.log('onToken', token);
+    },
   });
 
-  data.append({
-    text: 'Hello, how are you?',
-  });
-
-  return new StreamingTextResponse(stream, {}, data);
+  return new StreamingTextResponse(stream, {});
 }
